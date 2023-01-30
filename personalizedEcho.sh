56124c0603044ed45d6de31fe594a48b4fdbe740
Kanu Obinna <kanuchukwuemekaobinna@gmail.com>
2023-01-30 19:05:47 +0100

    Solution to Challenge 3: A Personalized Echo

---
+++ personalizedEcho.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Write a Bash script which accepts as input and displays the greeting "Welcome (name)"
+# https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo/problem?isFullScreen=true
+
+read -p "What's your name?" name
+
+echo "Welcome $name"
+
